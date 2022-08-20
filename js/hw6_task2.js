84993a26627bff70743fcc128925d0f1b0e32151
Igor Nedoshytko <46812129+winston4521@users.noreply.github.com>
2022-08-20 16:20:44 +0200

    поправки

--- js/hw6_task2.js
+++ js/hw6_task2.js
@@ -4,9 +4,9 @@ const ulEl = document.querySelector('#ingredients');
 const ingredEl = ingredients.map(elem => {
   const liEl = document.createElement('li');
   liEl.classList.add('item');
-  liEl.textContent = elem;
+  liEl.textContent = elem.textContent;
 
-  ulEl.appendChild(liEl);
-
-  console.log(elem);
+  return liEl;
 });
+console.log(ingredEl);
+ulEl.append(...ingredEl);
